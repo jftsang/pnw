4482cc624e944980a50cd51e418263a3c9e886dd
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2020-05-05 00:40:06 +0100

    Email daemon

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 API_PATH = "https://politicsandwar.com/api"
-API_KEY = "" # CHANGEME
+API_KEY = "CHANGEME"
 
 SLEEP_TIME = 1800
+
+
+SMTP_SERVER = "example.com"
+SMTP_PORT = 587  # For starttls
+SMTP_EMAIL = "you@example.com"
+SMTP_PASSWORD = "your_password"
