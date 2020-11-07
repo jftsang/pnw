d0b42bad87ee3e9a2586095114f575e7a99382fb
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2020-11-07 22:44:33 +0000

    pricedaemon.py

--- prices/pricedaemon.py
+++ prices/pricedaemon.py
@@ -3,17 +3,31 @@ import time
 from prices import update_price_history
 import subprocess
 from requests.exceptions import ConnectionError, Timeout
+import matplotlib.pyplot as plt
+import pandas as pd
+from config import *
 
 count = 0
-resources = ["steel", "gasoline"]
-while count < 48 * 14:
-    try:
-        for r in resources:
-            update_price_history(r)
-            subprocess.run(["gnuplot", "-e", f"resource='{r}'", "pricehist.gnuplot"])
-    except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
-        print("Couldn't update.")
+resources = ["coal", "oil", "iron", "steel", "gasoline"]
 
-    print("Waiting...")
-    time.sleep(1800)
-    count += 1
+def daemon():
+    while True:
+        try:
+            for r in resources:
+                update_price_history(r)
+                subprocess.run(["gnuplot", "-e", f"resource='{r}'", "pricehist.gnuplot"])
+        except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+            print("Couldn't update.")
+
+        print("Waiting...")
+        time.sleep(SLEEP_TIME)
+        count += 1
+
+
+def plot_prices(resource):
+    history = pd.read_csv(f"{resource}.csv")
+    breakpoint()
+    plt.plot(history)
+    outfile = "{resource}-new.png"
+    plt.savefig(outfile)
+
