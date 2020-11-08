ca61cebd6111440fae4f4e74b3dd8f6621320558
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2020-11-08 23:54:43 +0000

    Very lovely new plots with plotly

--- trades.py
+++ trades.py
@@ -23,21 +23,26 @@ def get_new_trades():
     return new_trades
 
 
-def update_trade_history():
-    new_trades = get_new_trades()
+def get_trade_history(update=True):
+    if update:
+        new_trades = get_new_trades()
     if os.path.isfile(TRADE_HISTORY):
         trade_history = pd.read_csv(TRADE_HISTORY,
                                     index_col=0,
                                     parse_dates=['date'],
                                     dtype={'volume': np.int64,
                                            'price': np.int64})
-        trade_history = trade_history.append(new_trades).drop_duplicates()
+        if update:
+            trade_history = trade_history.append(new_trades).drop_duplicates()
     else:
-        trade_history = new_trades
+        if update:
+            trade_history = new_trades
+        else:
+            raise FileNotFoundError
 
     trade_history = trade_history.sort_values('date')
     trade_history.to_csv(TRADE_HISTORY)
-    return trade_history, new_trades
+    return trade_history
 
 
 def get_current_prices(resource):
@@ -89,13 +94,60 @@ def makeplot(trades, current_prices, resource, ax=plt.gca()):
     ax.grid(True)
 
 
+def plotly(trades, current_prices, resource):
+    import plotly.express as px
+    import plotly.graph_objects as go
+
+    trades = trades[trades.resource == resource]
+    trades = trades[trades.date >= datetime.datetime.now() + relativedelta(days=-1)]
+    trades['logquantity'] = np.log2(trades.quantity)
+    sells = trades[trades.offer_type == 'sell']
+    buys = trades[trades.offer_type == 'buy']
+
+    fig = px.scatter(trades,
+            x="date",
+            y="price",
+            color="offer_type",
+            size="logquantity",
+            hover_data=["quantity"],
+            color_discrete_map={'buy': 'blue', 'sell': 'red'}
+            )
+    fig.add_trace(go.Scatter(
+        name="lowest sell offer",
+        x=[current_prices['lowestbuy']['date']],
+        y=[current_prices['lowestbuy']['price']],
+        mode="markers",
+        marker={'color': 'red', 'symbol': 'x'}
+        ))
+    fig.add_trace(go.Scatter(
+        name="highest buy offer",
+        x=[current_prices['highestbuy']['date']],
+        y=[current_prices['highestbuy']['price']],
+        mode="markers",
+        marker={'color': 'blue', 'symbol': 'cross'}
+        ))
+    ymin = min(trades.price.quantile(0.2),
+               current_prices['highestbuy']['price'])*0.95
+    ymax = max(trades.price.quantile(0.8),
+               current_prices['lowestbuy']['price'])*1.05
+    fig.update_layout(
+        title=resource,
+        autosize=False, width=800, height=600,
+        margin=dict(l=50, r=50, b=100, t=100, pad=4),
+        showlegend=True,
+        paper_bgcolor="LightSteelBlue")
+    fig.update_xaxes(title_text="date", dtick=7200000)
+    fig.update_yaxes(title_text="price", range=[ymin, ymax])
+    fig.write_html(f"plotly-{resource}.html", include_plotlyjs="cdn")
+
+
 def main(args):
     resources = [["credits", "coal", "oil", "uranium"],
                  ["lead", "iron", "bauxite", "gasoline"],
                  ["munitions", "steel", "aluminum", "food"]]
     while True:
         try:
-            trade_history, new_trades = update_trade_history()
+            trade_history = get_trade_history()
 
         except Exception as e:
             logging.exception(e)
@@ -112,14 +164,18 @@ def main(args):
                     current_prices = get_current_prices(resource)
                     makeplot(trade_history, current_prices, resource, ax)
                     plt.savefig(f'tradehist-{resource}.png')
+                    plt.close(fig.number)
 
                     plt.figure(fig_multi.number)
                     makeplot(trade_history, current_prices, resource, axs[i][j])
+
+                    plotly(trade_history, current_prices, resource)
                 except Exception as e:
                     logging.exception(e)
                     continue
         plt.figure(fig_multi.number)
         plt.savefig('tradehist-all.png')
+        plt.close(fig_multi.number)
         # plt.show()
         logging.info(f"Sleeping for {SLEEP_TIME}...")
         time.sleep(SLEEP_TIME)
@@ -133,3 +189,7 @@ if __name__ == "__main__":
     # parser.add_argument("resource")
     args = parser.parse_args()
     main(args)
+    resource='food'
+    plotly(get_trade_history(update=False),
+           get_current_prices(resource),
+           resource)
